ac1466ae38fc19993729f17dee6c4574caaf6f7e
FledgeXu <xzk0701@gmail.com>
2020-06-21 19:30:15 +0800

    1.15 版本第一次提交。

---
+++ src/main/java/com/github/tartaricacid/touhoulitlemaid/TouhouLittleMaid.java
@@ -0,0 +1,8 @@
+package com.github.tartaricacid.touhoulitlemaid;
+
+import com.github.tartaricacid.touhoulitlemaid.util.ModConstants;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(value = ModConstants.MOD_ID)
+public class TouhouLittleMaid {
+}
